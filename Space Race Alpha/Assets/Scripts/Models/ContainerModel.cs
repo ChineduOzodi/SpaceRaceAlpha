45b65b9ecda007207ac15cde00163437c8737fd6
chin24 <chin2413@gmail.com>
2016-10-04 15:13:09 -0500

    Start of the component system
    added a container component, and need to figure out how the the
    components will be attached to craft parts

---
+++ Space Race Alpha/Assets/Scripts/Models/ContainerModel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using CodeControl;
+/// <summary>
+/// Model that stores information for all container types
+/// </summary>
+public class ContainerModel : Model {
+
+    public ContainerTypes type;
+
+    public float massPerUnit;
+
+    public float maxAmount;
+    public float currentAmount;
+}
